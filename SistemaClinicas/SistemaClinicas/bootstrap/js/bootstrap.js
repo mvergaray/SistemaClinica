0f03110ad1c570933493b488614e92f89f8fb65c
Marco Antonio Vergaray Guerra <mvergaray@belatrix.com>
2017-07-19 09:45:42 -0500

    bootstrap

--- SistemaClinicas/SistemaClinicas/bootstrap/js/bootstrap.js
+++ SistemaClinicas/SistemaClinicas/bootstrap/js/bootstrap.js
@@ -1,5 +1,5 @@
 /*!
- * Bootstrap v3.3.4 (http://getbootstrap.com)
+ * Bootstrap v3.3.6 (http://getbootstrap.com)
  * Copyright 2011-2015 Twitter, Inc.
  * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
  */
@@ -17,7 +17,7 @@ if (typeof jQuery === 'undefined') {
 }(jQuery);
 
 /* ========================================================================
- * Bootstrap: transition.js v3.3.4
+ * Bootstrap: transition.js v3.3.6
  * http://getbootstrap.com/javascript/#transitions
  * ========================================================================
  * Copyright 2011-2015 Twitter, Inc.
